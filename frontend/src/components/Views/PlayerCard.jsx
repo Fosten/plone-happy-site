1a8a38698d0416ea178efc2efe6dbd38e6945d56
fosten <fosten@gmail.com>
2023-02-20 18:16:21 -0500

    Lint fix the PlayerCard view

--- frontend/src/components/Views/PlayerCard.jsx
+++ frontend/src/components/Views/PlayerCard.jsx
@@ -1,33 +1,30 @@
-import React from "react";
-import { DefaultView } from "@plone/volto/components";
+import React from 'react';
 import { Helmet } from '@plone/volto/helpers';
-import { Container, Header, Image, Segment } from 'semantic-ui-react';
+import { Container, Image, Segment } from 'semantic-ui-react';
 import { flattenToAppURL } from '@plone/volto/helpers';
 
 const PlayerCardView = (props) => {
   const { content } = props;
   return (
-      <Container>
+    <Container>
       <div id="page-document">
-      <Helmet title={content.title} />
-      <h1 className="documentFirstHeading">
-        {content.title}
-      </h1>
-      <Segment clearing>
-      <Image
-          src={flattenToAppURL(content.image?.scales?.preview?.download)}
-          size="small"
-          floated="right"
-          alt={content.image_caption}
-          avatar
-        />
-        <p>Birthdate: {content.birthdate}</p>
-        <p>Positions: {content.positions}</p>
-        <p>Current Team: {content.currentteam}</p>
-      <div dangerouslySetInnerHTML={{ __html: content.blurb.data }} />
-      </Segment>
+        <Helmet title={content.title} />
+        <h1 className="documentFirstHeading">{content.title}</h1>
+        <Segment clearing>
+          <Image
+            src={flattenToAppURL(content.image?.scales?.preview?.download)}
+            size="small"
+            floated="right"
+            alt={content.image_caption}
+            avatar
+          />
+          <p>Birthdate: {content.birthdate}</p>
+          <p>Positions: {content.positions}</p>
+          <p>Current Team: {content.currentteam}</p>
+          <div dangerouslySetInnerHTML={{ __html: content.blurb.data }} />
+        </Segment>
       </div>
-      </Container>
+    </Container>
   );
 };
 
