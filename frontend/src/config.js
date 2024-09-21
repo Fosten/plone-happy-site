7039c4a659214d50093909eadde3aa5610b70ed8
Fosten <fosten@gmail.com>
2024-09-21 13:46:28 -0400

    Use en-us language variant

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -21,7 +21,7 @@ export default function applyConfig(config) {
     ...config.settings,
     isMultilingual: false,
     supportedLanguages: ['en'],
-    defaultLanguage: 'en_US',
+    defaultLanguage: 'en-us',
     matomoSiteId: '1',
     matomoUrlBase: 'https://stats.happybaseball.com/',
   };
